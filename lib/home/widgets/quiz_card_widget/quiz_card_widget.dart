d6e43dff0f18eda3bcbcfc8e2403aa897e0d7f88
Eler <gl13eler@gmail.com>
2021-04-21 02:25:58 -0300

    Created the progress indicator as a shared widget

--- lib/home/widgets/quiz_card_widget/quiz_card_widget.dart
+++ lib/home/widgets/quiz_card_widget/quiz_card_widget.dart
@@ -1,4 +1,5 @@
 import 'package:devquiz/core/core.dart';
+import 'package:devquiz/shared/widgets/progress_indicator/progress_indicator_widget.dart';
 import 'package:flutter/material.dart';
 
 class QuizCardWidget extends StatelessWidget {
@@ -6,52 +7,42 @@ class QuizCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 24, left: 20, right: 20),
-      child: Container(
-        // height: 160,
-        // width: 177,
-        padding: EdgeInsets.all(16),
-        decoration: BoxDecoration(
-            border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
-            color: AppColors.white,
-            borderRadius: BorderRadius.circular(10)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              height: 40,
-              width: 40,
-              child: Image.asset(AppImages.blocks),
-            ),
-            SizedBox(height: 24),
-            Text(
-              "Gerenciamento de Estado",
-              style: AppTextStyles.heading15,
-            ),
-            SizedBox(height: 24),
-            Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Text(
-                    "3 de 10",
-                    style: AppTextStyles.body11,
-                  ),
+    return Container(
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+          border: Border.fromBorderSide(BorderSide(color: AppColors.border)),
+          color: AppColors.white,
+          borderRadius: BorderRadius.circular(10)),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 40,
+            width: 40,
+            child: Image.asset(AppImages.blocks),
+          ),
+          SizedBox(height: 21),
+          Text(
+            "Gerenciamento de Estado",
+            style: AppTextStyles.heading15,
+          ),
+          SizedBox(height: 21),
+          Row(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Text(
+                  "3 de 10",
+                  style: AppTextStyles.body11,
                 ),
-                Expanded(
-                  flex: 4,
-                  child: LinearProgressIndicator(
-                    value: 0.3,
-                    backgroundColor: AppColors.chartSecondary,
-                    valueColor:
-                        AlwaysStoppedAnimation<Color>(AppColors.chartPrimary),
-                  ),
-                )
-              ],
-            )
-          ],
-        ),
+              ),
+              Expanded(
+                flex: 2,
+                child: ProgressIndicatorWidget(value: 0.3)
+              )
+            ],
+          )
+        ],
       ),
     );
   }
